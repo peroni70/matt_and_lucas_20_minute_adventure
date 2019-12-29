e9bcf61b952293de8906c0a1fce9ff4f7e9eefb1
peroni70 <mattpep135@yahoo.com>
2019-12-29 03:22:38 -0500

    added maze_solver

---
+++ mazyboi/maze_solver.py
@@ -0,0 +1,22 @@
+class MazeRunner:
+
+    def __init__(self, maze=None):
+        if maze:
+            self.maze = maze
+
+    def solve_path(self,maze):
+        
+
+class Maze:
+
+    def __init__(self, chart, start, target):
+        self.chart = chart
+        self.start = start
+        self.target = target
+
+class Graph:
+
+    def __init__(self, chart, start):
+        
+
+
