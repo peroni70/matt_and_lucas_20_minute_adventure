230ff918aac09064948e33173b7684bad6cea5d9
Lucas Ou <lucasou1@bu.edu>
2019-12-31 02:00:17 -0500

    first working version

--- mazyboi/maze_solver.py
+++ mazyboi/maze_solver.py
@@ -10,21 +10,20 @@ class MazeRunner:
     def set_maze(self, maze):
         self.maze = maze
 
-    def solve_path(self,maze):
-        if self.maze == None:
+    def label_path(self):
+        if self.maze.chart == None:
             raise NameError('No Maze!')
-        n = len(maze.chart)
-        m = len(maze.chart[0])
+        n = len(self.maze.chart)
+        m = len(self.maze.chart[0])
 
         #create value_map
-        value_map = {i: 0 for i in enumerate(maze.target)}
-        target_set = set(maze.target)
+        value_map = {i: 0 for _, i in enumerate(self.maze.target)}
+        target_set = set(self.maze.target)
         for i in range(n):
             for j in range(m):
                 if (i,j) not in target_set:
-                    value_map = {(i,j): float('inf')}
-
-        queue = [(0, i) for _, i in enumerate(maze.target)]
+                    value_map[(i,j)] = float('inf')
+        queue = [(0, i) for _, i in enumerate(self.maze.target)]
         visited = set()
         while queue:
             value, v = heappop(queue)
@@ -32,15 +31,45 @@ class MazeRunner:
             #check four neighbors
             for _,i in enumerate([(0,1), (1,0), (-1,0), (0,-1)]):
                 neighbor = (v[0]+i[0], v[1]+i[1])
+                
                 #check if neighbor is in bounds
                 if neighbor[0] >=0 and neighbor[0] < n and neighbor[1] >= 0 and neighbor[1] < m:
                     #check if neighbor is in path 
-                    if maze.chart[neighbor[0]][neighbor[1]] == 0 and neighbor not in visited:
+                    if self.maze.chart[neighbor[0]][neighbor[1]] == 0 and neighbor not in visited:
                         if value_map[neighbor] > value + 1:
                             value_map[neighbor] = value + 1
                             heappush(queue, (value_map[neighbor], neighbor))
         return value_map
-                        
+
+    #find the shortest path
+    def find_path(self, value_map):
+        #dimensions of maze
+        n = len(self.maze.chart)
+        m = len(self.maze.chart[0])
+
+        min_dist = float('inf')
+        for _, i in enumerate(self.maze.start):
+            if value_map[i] < min_dist:
+                start_node = i
+        
+        current_node = start_node
+        path = [start_node]
+        
+        while current_node not in self.maze.target:
+            dist_next_node = float('inf')
+            
+            for _, i in enumerate([(0,1), (1,0), (-1,0), (0,-1)]):
+                neighbor = (current_node[0]+i[0], current_node[1]+i[1])
+                #check if neighbor is in bounds
+                if neighbor[0] >=0 and neighbor[0] < n and neighbor[1] >= 0 and neighbor[1] < m:
+                    if value_map[neighbor] < dist_next_node:
+                        dist_next_node = value_map[neighbor]
+                        next_node = neighbor
+            
+            path.append(next_node)
+            current_node = next_node
+
+        return path
 
 
 
@@ -56,4 +85,12 @@ class Graph:
     def __init__(self, chart, start):
         pass
 
-
+if __name__ == '__main__':
+    chart1 = [[1,0,0,1], [1,0, 0, 1], [1,0,1,1], [0, 0 , 1, 1]]
+    start1 = [(3,0)]
+    target1 = [(0,1), (0,2)]
+    maze1 = Maze(chart1, start1, target1)
+    solver1 = MazeRunner(maze1)
+    value_map1 = solver1.label_path()
+    path = solver1.find_path(value_map1)
+    print(path)
